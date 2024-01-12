f2d37043ce10b7a304b63029a4b3782b483a1c04
ashutoshrawat25 <ashutosh.rawat4@gmail.com>
2024-01-13 01:19:35 +0530

    basic markup created and sidebar selected tab functionality implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [selectedTab, setSelectedTab] = useState("Home");
+
+  return (
+    <div className="app-container">
+      <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      <div className="content">
+        <Header />
+        {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
